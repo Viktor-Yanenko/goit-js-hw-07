276cc66c7ec5a01cd3454b2ec8600b995c3a4db7
Viktor <viktoryanenko11@gmail.com>
2025-03-04 19:51:42 +0200

    fix minor bugs

--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@ const textOutput = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
   const name = event.currentTarget.value.trim();
   if (name === "") {
-    textOutput.textContent = "Anomymous";
+    textOutput.textContent = "Anonymous";
   } else {
     textOutput.textContent = name;
   }
