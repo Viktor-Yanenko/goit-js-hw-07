82ad29cddc1761591a9a10340b419bfa85f8c274
Viktor <viktoryanenko11@gmail.com>
2025-03-05 11:18:29 +0200

    fix tasks 2 and 6

--- js/task-2.js
+++ js/task-2.js
@@ -22,17 +22,13 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
 
-
-const gallery = document.querySelector('.gallery');
-
-images.forEach(image => {
-  const listItem = document.createElement('li')
-  const img = document.createElement('img');
-  img.src = image.url;
-  img.alt = image.alt;
-  listItem.append(img);
-  gallery.append(listItem);
-})
+const gallery = document.querySelector(".gallery");
+const galleryItems = images
+  .map((image) => {
+    return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryItems);
